baf6284bca720d16b763e96427044d72b2ac80ea
DEVA77A <devanand2005a@gmail.com>
2025-10-10 12:29:18 +0530

    grid

--- java.js
+++ java.js
@@ -1,4 +1,15 @@
-let a=2;
-let b=a;
-a=3;
-console.log(b);
+//let object1={age:20};   
+//let object2=object1;
+//object2.age=43;
+//console.log(object1.age); 
+
+//arrow function
+
+function add(a,b){
+    console.log(a+b);
+}
+add(10,34);
+const add2=(a,b)=>{
+    a+b;
+}       
+add(10,34);
