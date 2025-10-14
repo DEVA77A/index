54207a2a9dbb8fb044fae5989299c52a5ca7ede2
DEVA77A <devanand2005a@gmail.com>
2025-10-14 16:04:45 +0530

    functions

---
+++ memory stack.js
@@ -0,0 +1,31 @@
+// stack concept
+
+//  const mul = (a, b) => {
+//    return a * b;
+//  };  
+//  const square = (n) => {
+//    return mul(n, n);
+//  }   
+//  const print =(n) => {
+  
+//    console.log(square(n));
+//  };
+// print(4);
+// print(5);
+// print(6);
+
+
+
+
+// console.log('start');
+
+// //micro task
+// Promise.resolve().then(console.log('promise 1'));
+
+// //macro task
+// setTimeout(() => {
+//   console.log('timeout 1');
+// }, 10000);
+// setTimeout(() => {
+//   console.log('timeout 2');
+// }, 20000);
